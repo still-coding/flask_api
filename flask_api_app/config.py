755fddc49df63ce2ad0ea477698ec5c1410a0b5d
still-coding <devitsyn.ivan.nikolaevich@gmail.com>
2023-11-05 20:59:19 +0500

    add memcached testing workflow

--- flask_api_app/config.py
+++ flask_api_app/config.py
@@ -4,10 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="settings.env", extra="ignore")
 
-    mongodb_url: str
-    mongodb_dbname: str
-    redis_host: str
-    redis_port: int
+    # mongodb_url: str
+    # mongodb_dbname: str
+    # redis_host: str
+    # redis_port: int
     memcached_host: str
     memcached_port: int
 
