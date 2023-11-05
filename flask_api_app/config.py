fc9032aec57a51a37176046e9b935f9193c6b117
still-coding <devitsyn.ivan.nikolaevich@gmail.com>
2023-11-05 20:19:55 +0500

    fix workflow

--- flask_api_app/config.py
+++ flask_api_app/config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="settings.env", extra="ignore")
 
-    mongodb_url: str
-    mongodb_dbname: str
+    # mongodb_url: str
+    # mongodb_dbname: str
     redis_host: str
     redis_port: int
 
